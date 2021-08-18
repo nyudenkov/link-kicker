bc140a452800586439bf2790c3d8a51825757ae2
Nikita Y <fmajesty@pm.me>
2021-08-18 20:34:12 +0200

    Changed link mailing mechanism
    * Changed some messages

--- app/tasks/jobs.py
+++ app/tasks/jobs.py
@@ -10,12 +10,20 @@ from app.database.models import User
 from app.misc.sentry import capture_exception
 
 
+async def send_mailing_message(user):
+    message_text, markup = await get_random_link_message(user, mailing=True)
+    try:
+        await bot.send_message(user.tg_id, message_text, reply_markup=markup)
+    except aiogram_exceptions.BotBlocked:
+        logger.info(f"{user.tg_id} blocked bot")
+    except Exception as ex:  # noqa
+        capture_exception(ex)
+
+
 async def link_mailing():
-    for user in await User.filter(hour=datetime.now(pytz.timezone("Europe/Moscow")).hour):
-        message_text, markup = await get_random_link_message(user, mailing=True)
-        try:
-            await bot.send_message(user.tg_id, message_text, reply_markup=markup)
-        except aiogram_exceptions.BotBlocked:
-            logger.info(f"{user.tg_id} blocked bot")
-        except Exception as ex:  # noqa
-            capture_exception(ex)
+    now_hour: int = datetime.now(pytz.timezone("Europe/Moscow")).hour
+    for user in await User.filter(hour=now_hour):
+        await send_mailing_message(user)
+    if now_hour == 18:
+        for user in await User.filter(hour__isnull=True):
+            await send_mailing_message(user)
